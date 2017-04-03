4e70d9810a5ddad0357c47ecc42dc689979c7d8d
jiyanxin <13269326952@163.com>
2017-04-04 01:01:35 +0800

    登录注册子模块

---
+++ app/src/main/java/com/example/jiyanxin/login/User.java
@@ -0,0 +1,31 @@
+package com.example.jiyanxin.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by JIYANXIN on 2017/4/3.
+ */
+
+public class User extends AppCompatActivity {
+
+    private Button mReturnButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user);
+        mReturnButton = (Button)findViewById(R.id.returnback);
+
+    }
+    public void back_to_login(View view) {
+        //setContentView(R.layout.login);
+        Intent intent3 = new Intent(User.this,Login.class) ;
+        startActivity(intent3);
+        finish();
+
+    }
+}
